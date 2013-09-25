51b3e4725e6dd1a70f486b3cb5bd26f2be7fded5
Tom Armitage <tom@infovore.org>
2013-09-25 12:12:39 +0100

    Pages controller and default route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Ghostme::Application.routes.draw do
+  resources :pages do
+    get 'home'
+  end
+
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
