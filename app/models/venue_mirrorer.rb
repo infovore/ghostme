2d066436266a1c025a2f227f08d354c161b894b3
Tom Armitage <tom@infovore.org>
2013-09-28 11:29:51 +0100

    First pass at venue mirroring.

---
+++ app/models/venue_mirrorer.rb
@@ -0,0 +1,24 @@
+class VenueMirrorer
+  def self.mirror_venue_for_checkin(checkin)
+    # build a Foursquare object
+    foursquare = Foursquare::Base.new(checkin.user.access_token)
+
+    # get the relative latlng of this checkin
+    # add them to the user's offset_latlng to get the search terms
+    new_lat = checkin.user.offset_lat + checkin.relative_lat
+    new_lng = checkin.user.offset_lng + checkin.relative_lng
+
+    # search for venues near a space in the categories of this checkin
+    venues = foursquare.venues.nearby(:ll => "#{new_lat}, #{new_lng}", :categoryId => checkin.category_id_list)
+
+    # order them by proximity
+    venues = venues.sort_by do |venue|
+      lat_prox = (venue.location.lat - new_lat).abs
+      lng_prox = (venue.location.lng - new_lng).abs
+      [lat_prox, lng_prox]
+    end
+
+    # spit out the first one
+    venues.first
+  end
+end
