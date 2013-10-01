3066a90dc41705a82b7d55489de0c96314d38dfe
Tom Armitage <tom@infovore.org>
2013-10-01 19:27:54 +0100

    That's everything working from one worker, which is good for Heroku cheapness.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,24 @@
+require 'sidekiq'
+
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 60         # restarts workers that hang for 60 seconds
 
 before_fork do |server, worker|
-  # other settings
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
   end
 end
     
 after_fork do |server, worker|
-  # other settings
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
+
   if defined?(ActiveRecord::Base) 
     config = Rails.application.config.database_configuration[Rails.env]
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
