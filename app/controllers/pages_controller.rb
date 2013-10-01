b0d9b753562d6623ad7f6e9d1b13c0a93010cd18
Tom Armitage <tom@infovore.org>
2013-10-01 16:07:42 +0100

    Front-end for signing up that second account.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
-
+    foursquare = Foursquare::Base.new(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'])
+    @authorize_url = foursquare.authorize_url(callback_session_url)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,37 @@ class SessionsController < ApplicationController
 
   def callback
     foursquare = Foursquare::Base.new(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'])
-
     code = params[:code]
-    @access_token = foursquare.access_token(code, callback_session_url)
-    session[:access_token] = @access_token
 
-    @user = User.find_or_create_by_access_token(@access_token)
-    user_fs = Foursquare::Base.new(@access_token)
-    u = user_fs.users.find("self")
-    if @user.foursquare_id.blank?
-      @user.update_attributes(:firstname => u.first_name,
-                              :lastname => u.last_name,
-                              :foursquare_id => u.id,
-                              :picture_url=> u.photo)
-    end 
-    redirect_to pages_path
+    if !current_user
+      @access_token = foursquare.access_token(code, callback_session_url)
+      session[:access_token] = @access_token
+
+      @user = User.find_or_create_by_access_token(@access_token)
+      user_fs = Foursquare::Base.new(@access_token)
+      u = user_fs.users.find("self")
+      if @user.foursquare_id.blank?
+        @user.update_attributes(:firstname => u.first_name,
+                                :lastname => u.last_name,
+                                :foursquare_id => u.id,
+                                :picture_url=> u.photo)
+      end 
+      flash[:success] = "Your primary Foursquare account has been used for authentication, and linked to Ghostme."
+    else
+      @user = current_user
+
+      @secondary_access_token = foursquare.access_token(code, callback_session_url)
+      @user.secondary_access_token = @secondary_access_token
+
+      secondary_foursquare= Foursquare::Base.new(@secondary_access_token)
+      @secondary_user = secondary_foursquare.users.find("self")
+
+      @user.secondary_foursquare_id = @secondary_user.id
+
+      @user.save
+      flash[:success] = "Your secondary Foursquare account has been connected."
+    end
+    redirect_to root_path
   end
 
 
