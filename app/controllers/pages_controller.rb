6ff5ce51cea4597da145610b8469fee8ff72495a
Tom Armitage <tom@infovore.org>
2014-04-14 12:32:37 +0100

    New Foursquare stuff in these controllers.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
-    foursquare = Foursquare::Base.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'])
-    @authorize_url = foursquare.authorize_url(callback_session_url)
+    client = OAuth2::Client.new(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], :site => 'https://foursquare.com', :authorize_url => '/oauth2/authorize')
+
+    @authorize_url = client.auth_code.authorize_url(:redirect_uri => callback_session_url)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,31 @@ class SessionsController < ApplicationController
   def new
     return redirect_to user_path(current_user) if current_user
 
-    foursquare = Foursquare::Base.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'])
-    @authorize_url = foursquare.authorize_url(callback_session_url)
+    client = OAuth2::Client.new(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], :site => 'https://foursquare.com', :authorize_url => '/oauth2/authorize', :token_url => '/oauth2/access_token')
+
+    @authorize_url = client.auth_code.authorize_url(:redirect_uri => callback_session_url)
   end
 
   def callback
-    foursquare = Foursquare::Base.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'])
+    client = OAuth2::Client.new(ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'], :site => 'https://foursquare.com', :authorize_url => '/oauth2/authorize', :token_url => '/oauth2/access_token')
+
     code = params[:code]
 
     if !current_user
-      @access_token = foursquare.access_token(code, callback_session_url)
-      session[:access_token] = @access_token
+      @access_token = client.auth_code.get_token(code, :redirect_uri => callback_session_url)
 
-      @user = User.find_or_create_by_access_token(@access_token)
-      user_fs = Foursquare::Base.new(:access_token => @access_token)
-      u = user_fs.users.find("self")
+      session[:access_token] = @access_token.token
+
+      @user = User.find_or_create_by_access_token(@access_token.token)
+      user_fs = Foursquare2::Client.new(:oauth_token => @access_token.token, :api_version => '20140401')
+      u = user_fs.user('self')
+      p u
       if @user.foursquare_id.blank?
-        @user.update_attributes(:firstname => u.first_name,
-                                :lastname => u.last_name,
+        @user.update_attributes(:firstname => u.firstName,
+                                :lastname => u.lastName,
                                 :foursquare_id => u.id,
-                                :picture_url=> u.photo)
+                                :photo_prefix => u.photo.prefix,
+                                :photo_suffix => u.photo.suffix)
         CheckinIngester.delay.ingest_latest_checkins_for_user_id(@user.id)
 
         flash[:success] = "Your primary Foursquare account has been used for authentication, and linked to Ghostme. Your checkins are importing in the background."
@@ -31,11 +36,13 @@ class SessionsController < ApplicationController
     else
       @user = current_user
 
-      @secondary_access_token = foursquare.access_token(code, callback_session_url)
-      @user.secondary_access_token = @secondary_access_token
+      @secondary_access_token = client.auth_code.get_token(code, :redirect_uri => callback_session_url)
 
-      secondary_foursquare= Foursquare::Base.new(:access_token => @secondary_access_token)
-      @secondary_user = secondary_foursquare.users.find("self")
+      @user.secondary_access_token = @secondary_access_token.token
+
+      secondary_foursquare = Foursquare2::Client.new(:oauth_token => @secondary_access_token.token, :api_version => '20140401')
+
+      @secondary_user = secondary_foursquare.user("self")
 
       @user.secondary_foursquare_id = @secondary_user.id
 
